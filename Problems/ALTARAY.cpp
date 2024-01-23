0dbdc01da9aff5a1f0c2f31ee67538265bf7533e
DracoRX <145129888+DracoRX16@users.noreply.github.com>
2024-01-23 23:40:29 +0530

    Create ALTARAY.cpp

---
+++ Problems/ALTARAY.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;cin >> t;
+	while(t--){
+	    int n;cin>>n;
+	    int a[n];
+	    for(int i=0;i<n;i++) cin >> a[i];
+	    int dp[n];
+	    dp[n-1]=1;
+	    for(int i=n-2;i>=0;i--){
+	        if((a[i]>0 && a[i+1]<0) || (a[i+1]>0 && a[i]<0)){
+	            dp[i]=1+dp[i+1];
+	        }
+	        else{
+	            dp[i]=1;
+	        }
+	    }
+	    for(int i=0;i<n;i++) cout << dp[i] << " ";
+	    cout << endl;
+	}
+
+}
